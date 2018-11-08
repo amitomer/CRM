d8bcba5f4a6fcdb5e368d02aeefe8f792c6914c0
amitomer <amitomer312@gmail>
2018-11-08 09:16:08 +0200

    heroku plz

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     next()
 })
+app.use(express.static('build'));
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(bodyParser.json());
