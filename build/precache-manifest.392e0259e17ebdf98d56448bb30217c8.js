a567a7df5eb715be247c6c50485d92aa9fe99965
amitomer <amitomer312@gmail>
2018-11-08 09:35:38 +0200

    commit

---
+++ build/precache-manifest.392e0259e17ebdf98d56448bb30217c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96136c780aa5cf8b7c03",
+    "url": "/static/js/main.96136c78.chunk.js"
+  },
+  {
+    "revision": "bc31a1d4b0db0aa6f17e",
+    "url": "/static/js/1.bc31a1d4.chunk.js"
+  },
+  {
+    "revision": "96136c780aa5cf8b7c03",
+    "url": "/static/css/main.1c78b446.chunk.css"
+  },
+  {
+    "revision": "609934cddd731ddbaf2ea5a0d45ea577",
+    "url": "/index.html"
+  }
+];
